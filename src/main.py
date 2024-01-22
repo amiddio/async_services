1c5f94f14362c67892d1cd0a5c0d359ee16a3266
d.shkinin <amiddio.dev@gmail.com>
2024-01-22 18:15:42 +0400

    Added environment variable file

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
+import os
+from dotenv import load_dotenv
+
 from aiohttp import web
 
 from routes import routes
 
-PORT = 8008
+load_dotenv()
 
 app = web.Application()
 app.add_routes(routes)
 
 if __name__ == '__main__':
-    web.run_app(app, port=PORT)
+    web.run_app(app, port=int(os.environ.get('PORT')))
